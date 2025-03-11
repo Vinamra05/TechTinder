0dc9a6f81b9d19b843f4542395efabc08ac34e05
Vinamra Sharma <vinamrasharma0523@gmai.com>
2025-03-11 20:32:35 +0530

    filtering on skills

--- src/routes/user.js
+++ src/routes/user.js
@@ -54,35 +54,79 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
-userRouter.get("/feed", userAuth, async (req, res) => {
-  try {
+// userRouter.get("/feed", userAuth, async (req, res) => {
+//   try {
 
+//     const loggedInUser = req.user;
+//     const page = parseInt(req.query.page) || 1;
+//     let limit = parseInt(req.query.limit) || 10;
+//     limit =  limit > 50 ? 50 :limit;
+//     const skip = (page-1)*limit;
+
+//     const connectionRequests = await ConnectionRequestModel.find({
+//       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+//     }).select("fromUserId  toUserId");
+
+//     const hideUsersFromFeed = new Set();
+//     connectionRequests.forEach((request) => {
+//       hideUsersFromFeed.add(request.toUserId.toString());
+//       hideUsersFromFeed.add(request.fromUserId.toString());
+//     });
+//     const users = await UserModel.find({
+//       $and:[
+//         {_id: { $nin: Array.from(hideUsersFromFeed) }},
+//         { _id: { $ne: loggedInUser._id } },
+//       ]
+//     }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+//     res.json({data:users});
+//   } catch (error) {
+//     res.status(400).send({
+//       message: error.message,
+//     });
+//   }
+// });
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  //modified api by me
+  try {
     const loggedInUser = req.user;
     const page = parseInt(req.query.page) || 1;
     let limit = parseInt(req.query.limit) || 10;
-    limit =  limit > 50 ? 50 :limit;
-    const skip = (page-1)*limit;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    //added by my own for the filter search for specfic skills user will have similar if apply filter
+    const skillFilter = req.query.skills ? req.query.skills.split(",") : [];
 
     const connectionRequests = await ConnectionRequestModel.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
-    }).select("fromUserId  toUserId");
+    }).select("fromUserId toUserId");
 
     const hideUsersFromFeed = new Set();
     connectionRequests.forEach((request) => {
       hideUsersFromFeed.add(request.toUserId.toString());
       hideUsersFromFeed.add(request.fromUserId.toString());
     });
-    const users = await UserModel.find({
-      $and:[
-        {_id: { $nin: Array.from(hideUsersFromFeed) }},
-        { _id: { $ne: loggedInUser._id } },
-      ]
-    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
-    res.json({data:users});
+
+    let filterConditions = [
+      { _id: { $nin: Array.from(hideUsersFromFeed) } },
+      { _id: { $ne: loggedInUser._id } },
+    ];
+
+    if (skillFilter.length > 0) {
+      filterConditions.push({
+        skills: { $in: skillFilter.map((skill) => new RegExp(skill, "i")) }, // yaha i ka matlb case insensitive searching hogi caps m aye query ya small m it will search by as we have made a new reg exp with the i flag
+      });
+    }
+
+    const users = await UserModel.find({ $and: filterConditions })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
   } catch (error) {
     res.status(400).send({
       message: error.message,
     });
   }
 });
- 
